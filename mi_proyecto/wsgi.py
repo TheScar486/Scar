6382c18ddd6de74da9f508e8c7b78ac45e9a7b8a
TheScar486 <solorzanooscar486@gmail.com>
2025-04-08 12:46:53 -0600

    Definir BASE_DIR en wsgi.py para solucionar el error

--- mi_proyecto/wsgi.py
+++ mi_proyecto/wsgi.py
@@ -9,10 +9,15 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
 from whitenoise import WhiteNoise
+
+BASE_DIR = settings.BASE_DIR  # Importa BASE_DIR desde settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mi_proyecto.settings')
 
 application = get_wsgi_application()
 application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
+
+
 
